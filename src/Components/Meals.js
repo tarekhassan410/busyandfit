3b98c5a8eb805f0ca5341668f2d828a7e21a77c1
tarekhassan410 <tarekhassan410@gmail.com>
2018-12-11 03:02:35 +0200

    .box .paddingless added

--- src/Components/Meals.js
+++ src/Components/Meals.js
@@ -4,13 +4,15 @@ const Meals = ({ meals }) => (
   <div className="columns is-multiline is-mobile">
     {meals.map(meal => (
       <div key={meal.id} className="column is-3 ">
-        <div className="card">
+        <div className="card box is-paddingless">
           <div className="card-image">
             <figure className="image is-4by3">
+              <a href={meal.URL}>
               <img
                 src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
                 alt=""
               />
+              </a>
             </figure>
           </div>
           <div className="card-content">{meal.URL}</div>
